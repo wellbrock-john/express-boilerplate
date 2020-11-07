9db6ce6888c692e2bb65a8d9c79a5cfb304aede1
John Wellbrock <wellbrock23john@gmail.com>
2020-11-06 19:37:23 -0700

    updated to use bearer tokens and new middleware

--- src/app.js
+++ src/app.js
@@ -1,33 +1,29 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+app.use(
+	morgan(NODE_ENV === "production" ? "tiny" : "common", {
+		skip: () => NODE_ENV === "test",
+	})
+);
+app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+//app.use("/api/someRoute", someRoutesRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+app.get("/", (req, res) => {
+	res.send("Hello, world!");
+});
 
-  app.use(function errorHandler(error, req, res, next) {
-      let response
-      if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-      } else {
-        console.error(error)
-        response = { message: error.message, error }
-      }
-      res.status(500).json(response)
-    })
+app.use(errorHandler);
 
-module.exports = app
+module.exports = app;
